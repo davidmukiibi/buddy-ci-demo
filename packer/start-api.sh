a3561a51913cc85a9dc2576ff2047ce3006b670b
David Mukiibi <david.mukiibiq@gmail.com>
2020-06-17 13:30:53 +0300

    renamed this from prvious commit

---
+++ packer/start-api.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+export_env_variables() {
+    export APP_SECRET=iigMnxLgWdFJppmd3JgZRzOUYLgmjaXX
+    export REQUESTS_ORIGIN=*
+    export TEST_DATABASE_URL=postgresql://postgres@localhost:5432/reactdb
+    export DATABASE_URL=postgresql://postgres@localhost:5432/reactdb
+}
+
+run_application_start_command() {
+    python run.py
+}
+
+main() {
+    export_env_variables
+
+    run_application_start_command
+}
+
+main "$@"
