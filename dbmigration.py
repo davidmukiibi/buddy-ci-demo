da5a3ebdf5b45ffda7a6e97510d314b3c3e487ba
David Mukiibi <david.mukiibiq@gmail.com>
2020-06-23 17:36:28 +0300

    mport migratcommand

--- dbmigration.py
+++ dbmigration.py
@@ -1,7 +1,8 @@
 from flask_script import Manager
 from bucketlist import create_app
+from flask_migrate import MigrateCommand
 
-app = create_app()
+app = create_app('testing')
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
