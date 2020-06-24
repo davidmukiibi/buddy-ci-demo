7c7e60cd17032057ef71d35c1e4f8423565ac545
David Mukiibi <david.mukiibiq@gmail.com>
2020-06-24 18:10:54 +0300

    change postgres version

--- packer/setup.sh
+++ packer/setup.sh
@@ -11,7 +11,7 @@ install_postgresql() {
     sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' && \
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && \
     apt-get update && \
-    apt-get install postgresql-12 -y
+    apt-get install postgresql -y
 }
 
 create_database_user() {
